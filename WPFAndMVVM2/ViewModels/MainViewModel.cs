4ba9b1eed788f58b2deb6d274e0b6112465fd1df
kochjar <timgfx@outlook.com>
2023-05-02 17:04:04 +0200

    Add project files.

---
+++ WPFAndMVVM2/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel
+    {
+        private PersonRepository personRepo = new PersonRepository();
+
+        // Implement the rest of this MainViewModel class below to 
+        // establish the foundation for data binding !
+
+        public PersonViewModel currentPerson { get; set; }
+        public List<PersonViewModel> PersonsVM { get; set; }
+        public MainViewModel()
+        { 
+            PersonsVM = new List<PersonViewModel>();
+            foreach (Person person in personRepo.GetAll())
+            {
+                PersonViewModel personViewModel= new PersonViewModel(person);
+                PersonsVM.Add(personViewModel);
+            }
+        }
+       
+
+    }
+}
---
+++ WPFAndMVVM2/ViewModels/PersonViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person person;
+
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string phone;
+        public string FirstName {
+            get { return firstName; } 
+            set { 
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            } 
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+                OnPropertyChanged("Phone");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged != null) propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public PersonViewModel(Person person)
+        {
+            this.person = person;
+            this.FirstName = person.FirstName;
+            this.LastName = person.LastName;
+            this.Age = person.Age;
+            this.Phone = person.Phone;
+        }
+    }
+}
